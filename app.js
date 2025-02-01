3a505c849c2d35b0cd3189b3bfe67b67c46cdf2e
zaid amjad <h.zaidamjad29@gmail.com>
2025-02-01 14:13:38 +0500

    security features

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
+const rateLimit = require('express-rate-limit');
 const express = require('express');
 const morgan = require('morgan');
+const helmet = require('helmet');
+const sanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 const app = express();
 
 const tourRouter = require('./Routers/tourRouter');
@@ -7,11 +12,27 @@ const userRouter = require('./Routers/userRouter');
 const AppError = require('./utils/AppError');
 const globalErrorHandler = require('./controllers/errorController');
 
-app.use(express.json());
+app.use(helmet());
+
+app.use(express.json({ limit: '10kb' }));
+
+app.use(sanitize());
+
+app.use(xss());
+
+app.use(hpp());
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from the same IP, please try again in an hour!',
+});
+
+app.use('/api', limiter);
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
